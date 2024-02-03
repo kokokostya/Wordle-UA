d2c575373c6c5657042ce5dafbc459fe8ed41fea
kokokostya <working.kostya@gmail.com>
2024-02-03 19:14:36 +0100

    User fix

--- js/app.js
+++ js/app.js
@@ -241,9 +241,22 @@ function App(props) {
   React.useEffect(function () {
     localStorage.setItem("UID", JSON.stringify(UID));
     // Fix individual user's stats
-    // if (UID == "lrn4kdwy1laxgo014" && stats.games <= 219) {
-    //   localStorage.setItem("stats", JSON.stringify({games:219,won:216,streak:101,maxStreak:101,attempts:{1:0,2:5,3:47,4:72,5:67,6:26}}));
-    // }
+    if (UID == "ls5sigsj18sfrc9tr" && stats.games < 393) {
+      localStorage.setItem("stats", JSON.stringify({
+        games: 393,
+        won: 393,
+        streak: 393,
+        maxStreak: 393,
+        attempts: {
+          1: 0,
+          2: 26,
+          3: 107,
+          4: 180,
+          5: 59,
+          6: 21
+        }
+      }));
+    }
   }, [UID]);
 
   // Update theme and save to local storage
