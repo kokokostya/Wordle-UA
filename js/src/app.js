e8adb06f962e7605ea91d4271d14a41508a16d56
kokokostya <working.kostya@gmail.com>
2025-01-05 20:37:04 +0100

    Upd

--- js/src/app.js
+++ js/src/app.js
@@ -140,18 +140,18 @@ function App(props) {
     window.history.replaceState({}, "", url);
 
     // Fix individual user's stats
-    const localUID = tryLoadingFromLocalStorage("UID", UID, {skipSetting: true, ignoreLettersLimit: true});
-    if (currentEdition.lettersLimit == 5) {
-      if (localUID == "lc0yofsc2ujwuv554" && localStats.games == 1079) {
-        localStats.games = getIssueNumber(5) - 322;
-        localStats.won = getIssueNumber(5) - 322;
-        localStats.streak = getIssueNumber(5) - 322;
-        localStats.maxStreak = getIssueNumber(5) - 322;
-        localStats.attempts[3] = getIssueNumber(5) - 322 - localStats.attempts[1] - localStats.attempts[2] - localStats.attempts[4] - localStats.attempts[5] - localStats.attempts[6];
-        setStats(localStats);
-        saveToLocalStorage("stats", stats);
-      }
-    }
+    // const localUID = tryLoadingFromLocalStorage("UID", UID, {skipSetting: true, ignoreLettersLimit: true});
+    // if (currentEdition.lettersLimit == 5) {
+    //   if (localUID == "lc0yofsc2ujwuv554" && localStats.games == 1079) {
+    //     localStats.games = getIssueNumber(5) - 322;
+    //     localStats.won = getIssueNumber(5) - 322;
+    //     localStats.streak = getIssueNumber(5) - 322;
+    //     localStats.maxStreak = getIssueNumber(5) - 322;
+    //     localStats.attempts[3] = getIssueNumber(5) - 322 - localStats.attempts[1] - localStats.attempts[2] - localStats.attempts[4] - localStats.attempts[5] - localStats.attempts[6];
+    //     setStats(localStats);
+    //     saveToLocalStorage("stats", stats);
+    //   }
+    // }
 
     // Keep track of time and reset once new game is out
     timer = setInterval(() => {
